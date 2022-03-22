11e502b63d4ba0da232e1e3f02bee3493be5884a
Cierra Morris <4cibear@gmail.com>
2022-03-21 21:07:24 -0600

    standardized project slides

--- src/components/ProjectSlider.js
+++ src/components/ProjectSlider.js
@@ -4,11 +4,10 @@ import { GITHUB_USERNAME } from '../root.link';
 
 const ProjectSlider = () => {
     let slides = projects.map((project, index) => (
-        <article className='flex flex-col  h-max  bg-white' key={index} >
-            <picture className={index % 2 === 0 ? 'w-full shadow-lg ' : 'w-full shadow-lg '}>
-                <img src={project.image} alt={project.title} />
-            </picture>
-            <div className='flex flex-col overflow-auto  space-y-3 my-3 mx-1 w-full '>
+        <article className='flex flex-col h-max  bg-white ' key={index} >
+            <img src={project.image} alt={project.title} className="portfolio-img" />
+
+            <div className='flex flex-col overflow-visible  space-y-3  w-full p-2'>
                 <h3 className='uppercase font-bold text-lg'>{project.title}</h3>
                 <p>{project.description}</p>
                 <div className='flex overflow-auto space-x-3 pb-2 '>
@@ -33,12 +32,13 @@ const ProjectSlider = () => {
 
 
     return (
-        <section className='my-28 px-5 projectSlider' id='projects'> 
+        <section className='my-28 px-5 projectSlider' id='projects'>
             <header className='text-2xl font-bold pt-10'>
                 <h2>Projects</h2>
+                <p className='text-sm'>(Swipe to view next)</p>
             </header>
             <div className='my-7 space-y-24'>
-                <Carousel slides={slides} autoplay={false} arrows={true}/>
+                <Carousel slides={slides} autoplay={false} arrows={true} />
             </div>
         </section>
     )
