26e119ce69142ca5accaac49c2a865a318ffba83
Cierra Morris <4cibear@gmail.com>
2022-03-21 21:42:45 -0600

    fixed broken link

--- src/components/Header.js
+++ src/components/Header.js
@@ -45,7 +45,7 @@ export default function Header() {
                     Contact
                   </a>
                   <a
-                    href="#"
+                    href="http://drive.google.com"
                     className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-lg font-medium"
                   >
                     Resume
@@ -121,7 +121,7 @@ export default function Header() {
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                href="#"
+                href="http://drive.google.com"
                 className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
               >
                 Resume
