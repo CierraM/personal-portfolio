88d2b6660e7b810eaceb67b67329d89a3a5b1595
Cierra Morris <4cibear@gmail.com>
2022-12-28 13:09:41 -0700

    updated work history

--- src/ExData.js
+++ src/ExData.js
@@ -1,8 +1,21 @@
 const workHistory = [
     {
+        companyName: 'The Church of Jesus Christ of Latter Day Saints',
+        role: 'Software Developer',
+        duration: 'Apr 2022 - Present',
+        workDes: [
+            'Created software features and bug fixes according to specifications',
+            'Stabilized complex legacy code by writing unit and integration tests',
+            'Collaborated with a large team using scrum/agile methods',
+            'Worked with C#/.NET Core, React, Typescript, and SQL Server database'
+
+        ],
+        exposer: ['React', "C#/.Net", 'Sql Server', 'Unit Testing', 'Agile Development']
+    },
+    {
         companyName : 'BYU-Idaho Computer Science',
         role: 'Teaching Assistant',
-        duration: "Aug 2020 – Present",
+        duration: 'Aug 2020 – Apr 2022',
         workDes: [
             'Kept track of student progress and grading',
             'Gave feedback to students on assignments and projects',
